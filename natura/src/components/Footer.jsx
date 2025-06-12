605376e2a3ac98c0d0b1b9289c7110082a3a9c7f
Matias Garnica <victormatiasgarnica@gmail.com>
2025-06-12 19:03:14 -0300

    footer

--- natura/src/components/Footer.jsx
+++ natura/src/components/Footer.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      
+      <div>
+        <div>
+          <h4> Sobre Natura</h4>
+          <ul>
+            <li><a href="#">Bien estar bien</a></li>
+            <li><a href="#">Sustentabilidad</a></li>
+          </ul>
+        </div>
+        <div>
+          <h4>Ayuda</h4>
+          <ul>
+            <li>Encontra Natura</li>
+            <li>Ayuda y Contacto</li>
+            <li>Defensa de las y los Consumidores, para reclamos ingrese aquí</li>
+            <li>Alertas y Retiro de Productos</li>
+            <li><button> Me Arrepenti de mi pedido</button></li>
+            
+          </ul>
+        <div>
+          <h1>Soporte</h1>
+          <ul>
+            <li>Terminos y condiciones</li>
+            <li>Politica de Privacidad</li>
+            <li>Politica de Cookies</li>
+          </ul>
+        </div>
+      </div>
+      </div>
     </div>
   )
 }
