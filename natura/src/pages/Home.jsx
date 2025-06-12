3f589d10b2d6a698be91b5ceb927bef6e362bdc6
denisesancheez <you@example.com>
2025-06-12 20:20:48 -0300

    agrego secciones footer

--- natura/src/pages/Home.jsx
+++ natura/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from '../components/Header'
 import Footer from "../components/Footer"
 
+
 const Home = () => {
   return (
     <div>
       <Header/>
-      <Footer/>
+      
+       <Footer/>
     </div>
   )
 }
