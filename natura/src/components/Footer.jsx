f21558ec35aff96103f0b4c606d6814742c4acc2
denisesancheez <you@example.com>
2025-06-12 22:14:11 -0300

    agrego css de footer

--- natura/src/components/Footer.jsx
+++ natura/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faFacebookF,
@@ -8,67 +8,74 @@ import {
   faYoutube,
   faLinkedinIn
 } from '@fortawesome/free-brands-svg-icons';
+
+import visa from '../assets/visa.png';
+import mastercard from '../assets/mastercard.png';
+import amex from '../assets/amex.png';
+import cabal from '../assets/cabal.png';
+import naranja from '../assets/naranja.png';
+import diners from '../assets/diners.png';
+
 import './styles/footer.css';
-
-
 
 const Footer = () => {
   return (
+    <footer className="footer">
+      <div className="footer-top">
+        <div className="footer-column">
+          <h4>sobre natura</h4>
+          <ul>
+            <li>bien estar bien</li>
+            <li>sustentabilidad</li>
+          </ul>
+        </div>
 
-    <div className='footer'>
-      <section className='1footer'>
-        <div>
-          <h4> Sobre Natura</h4>
+        <div className="footer-column">
+          <h4>ayuda</h4>
           <ul>
-            <li href="#">Bien estar bien</li>
-            <li href="#">Sustentabilidad</li>
+            <li>encontrá natura</li>
+            <li>ayuda y contacto</li>
+            <li>defensa de las y los consumidores, para reclamos ingrese aquí</li>
+            <li>defensa de las y los consumidores, ingrese aquí</li>
+            <li>alertas y retiro de productos</li>
           </ul>
+          <button className="footer-button">me arrepentí del pedido</button>
         </div>
-        <div>
-          <h4>Ayuda</h4>
-          <ul>
-            <li>Encontra Natura</li>
-            <li>Ayuda y Contacto</li>
-            <li>Defensa de las y los Consumidores, para reclamos ingrese aquí</li>
-            <li>Alertas y Retiro de Productos</li>
-            <li><button> Me Arrepenti de mi pedido</button></li>
-          </ul>
-        </div>
-        <div>
-          <h1>Soporte</h1>
-          <ul>
-            <li>Terminos y condiciones</li>
-            <li>Politica de Privacidad</li>
-            <li>Politica de Cookies</li>
-          </ul>
-        </div>
-      </section>
 
-      <section >
-        <div className='footer_pagoss'>
-          <h4>Medios de pago </h4>
-          <div>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+        <div className="footer-column">
+          <h4>soporte</h4>
+          <ul>
+            <li>términos y condiciones</li>
+            <li>políticas de privacidad</li>
+            <li>política de cookies</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <div className="footer-payments">
+          <h5>medios de pago</h5>
+          <div className="payment-icons">
+            <img src={visa} alt="Visa" />
+            <img src={mastercard} alt="Mastercard" />
+            <img src={diners} alt="Diners" />
+            <img src={amex} alt="Amex" />
+            <img src={cabal} alt="Cabal" />
+            <img src={naranja} alt="Naranja" />
           </div>
         </div>
-      <div className="footer_redes">
-        <FontAwesomeIcon icon={faFacebookF} size="2x" />
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
-        <FontAwesomeIcon icon={faXTwitter} size="2x" />
-        <FontAwesomeIcon icon={faTiktok} size="2x" />
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
-        <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+
+        <div className="footer-social">
+          <FontAwesomeIcon icon={faFacebookF} />
+          <FontAwesomeIcon icon={faInstagram} />
+          <FontAwesomeIcon icon={faXTwitter} />
+          <FontAwesomeIcon icon={faTiktok} />
+          <FontAwesomeIcon icon={faYoutube} />
+          <FontAwesomeIcon icon={faLinkedinIn} />
+        </div>
       </div>
-      </section>
+    </footer>
+  );
+};
 
-    </div>
-
-  )
-}
-
-export default Footer
+export default Footer;
