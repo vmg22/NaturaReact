1d62fbff2bb99766a8b51f1847ce3554a3e831d7
Matias Garnica <victormatiasgarnica@gmail.com>
2025-06-12 22:34:46 -0300

    carousel1

--- natura/src/components/carrousel/CarrouselCard1.jsx
+++ natura/src/components/carrousel/CarrouselCard1.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Banner_3_DESK from "../../assets/ARTICULOS-IMG/Banner_3_DESK.jpg";
+import EKOSACAI1_Banner_2_Desk_1920x832_THUMB from "../../assets/ARTICULOS-IMG/EKOSACAI1_Banner_2_Desk_1920x832_THUMB.jpg";
+import "../styles/carrouseleCard1.css";
 
 const CarrouselCard1 = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={Banner_3_DESK} alt="First slide" />
+          <Carousel.Caption>
+            <h3>COMPRA CON 35% OFF POR LA APP!</h3>
+            <p>30% OFF a partir de $55.000 si compras por APP, tenes un +5% EXTRA. cupon: MAS30</p>
+          </Carousel.Caption>
+        </Carousel.Item>
 
-export default CarrouselCard1
+        <Carousel.Item>
+          <img className="d-block w-100" src={EKOSACAI1_Banner_2_Desk_1920x832_THUMB} alt="Second slide" />
+          <Carousel.Caption>
+            <h3>REGALO POR COMPRAS MAYORES A $60.000!</h3>
+            <p>Con tu compra mayor a $60.000 te llevas un hidratante corporal de regalo!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default CarrouselCard1;
--- natura/src/pages/Home.jsx
+++ natura/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Header from '../components/Header'
 import Footer from "../components/Footer"
+import CarrouselCard1 from '../components/carrousel/CarrouselCard1'
 
 const Home = () => {
   return (
     <div>
       <Header/>
-      home
+      <CarrouselCard1/>
       <Footer/>
     </div>
   )
