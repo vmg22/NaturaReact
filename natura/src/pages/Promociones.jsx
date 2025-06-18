7a8881e17ea858d0c75a2f574371b8ae086e6a3d
Denisesancheez <denisesofiasanchez2@gmail.com>
2025-06-17 22:18:11 -0300

    btn promos

--- natura/src/pages/Promociones.jsx
+++ natura/src/pages/Promociones.jsx
@@ -3,38 +3,54 @@ import { Link } from 'react-router-dom'
 import CategoriaGeneral from '../components/CategoriaGeneral'
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import "../styles/Promociones.css" 
+
 const Promociones = () => {
-    let categoria = "promociones"
+  let categoria = "promociones"
+
   return (
     <div>
-      <Header/>
+      <Header />
       <CategoriaGeneral categoria={categoria} />
-      <div className="d-flex justify-content-center">
-        <div>
-          <Link to="/pro-descuentos">
-            <button className="btn btn-light mx-2">Descuentos</button>
-          </Link>
-        </div>
-        <div>
-          <Link to="/pro-kits-natura">
-            <button className="btn btn-light mx-2">Kits Natura</button>
-          </Link>
-        </div>
-        <div>
-          <Link to="/pro-primera-compra">
-            <button className="btn btn-light mx-2">Primera compra</button>
-          </Link>
-        </div>
-        <div>
-          <Link to="/pro-lanzamientos">
-            <button className="btn btn-light mx-2">Lanzamientos</button>
-          </Link>
-        </div>
+
+      <div className="tipos-promocion-container">
+        <Link to="/pro-descuentos" className="tipo-promocion-btn">
+          <img
+            src="https://production.na01.natura.com/on/demandware.static/-/Sites-NatArgentina-Library/default/dw382f0915/NE%20Home/Promoc%CC%A7o%CC%83es.png"
+            alt="Descuentos"
+            className="icono-img-promociones"
+          />
+          <span>Descuentos</span>
+        </Link>
+        <Link to="/pro-kits-natura" className="tipo-promocion-btn">
+          <img
+            src="https://production.na01.natura.com/on/demandware.static/-/Sites-NatArgentina-Library/default/dw382f0915/NE%20Home/Promoc%CC%A7o%CC%83es.png"
+            alt="Kits Natura"
+            className="icono-img-promociones"
+          />
+          <span>Kits Natura</span>
+        </Link>
+        <Link to="/pro-primera-compra" className="tipo-promocion-btn">
+          <img
+            src="https://production.na01.natura.com/on/demandware.static/-/Sites-NatArgentina-Library/default/dwe5a8c7ac/NE%20Home/Presentes.png"
+            alt="Primera compra"
+            className="icono-img-promociones"
+          />
+          <span>Primera compra</span>
+        </Link>
+        <Link to="/pro-lanzamientos" className="tipo-promocion-btn">
+          <img
+            src="https://production.na01.natura.com/on/demandware.static/-/Sites-NatArgentina-Library/default/dw382f0915/NE%20Home/Promoc%CC%A7o%CC%83es.png"
+            alt="Lanzamientos"
+            className="icono-img-promociones"
+          />
+          <span>Lanzamientos</span>
+        </Link>
       </div>
-      <Footer/>
+
+      <Footer />
     </div>
   )
 }
 
 export default Promociones
-
--- natura/src/pages/Rostro.jsx
+++ natura/src/pages/Rostro.jsx
@@ -16,19 +16,19 @@ const Rostro = () => {
       <CategoriaGeneral categoria={categoria} />  
       <div className="tipos-piel-container">
         <Link to="/r-piel-oleosa" className="tipo-piel-btn">
-          <img src="https://production.na01.natura.com/on/demandware.static/-/Sites-NatArgentina-Library/default/dwce1a5e1c/NE%20Rostro/Pele%20Oleosa.png" alt="Piel Oleosa" className="icono-img" />
+          <img src="https://production.na01.natura.com/on/demandware.static/-/Sites-NatArgentina-Library/default/dwce1a5e1c/NE%20Rostro/Pele%20Oleosa.png" alt="Piel Oleosa" className="icono-img-rostro" />
           <span>piel oleosa</span>
         </Link>
         <Link to="/r-piel-seca" className="tipo-piel-btn">
-          <img src="https://production.na01.natura.com/on/demandware.static/-/Sites-NatArgentina-Library/default/dw43d48bf0/NE%20Rostro/Pele%20Seca.png" alt="Piel Seca" className="icono-img" />
+          <img src="https://production.na01.natura.com/on/demandware.static/-/Sites-NatArgentina-Library/default/dw43d48bf0/NE%20Rostro/Pele%20Seca.png" alt="Piel Seca" className="icono-img-rostro" />
           <span>piel seca</span>
         </Link>
         <Link to="/r-piel-mixta" className="tipo-piel-btn">
-          <img src="https://production.na01.natura.com/on/demandware.static/-/Sites-NatArgentina-Library/default/dw5e0cfeee/NE%20Rostro/Pele%20Mista.png" alt="Piel Mixta" className="icono-img" />
+          <img src="https://production.na01.natura.com/on/demandware.static/-/Sites-NatArgentina-Library/default/dw5e0cfeee/NE%20Rostro/Pele%20Mista.png" alt="Piel Mixta" className="icono-img-rostro" />
           <span>piel mixta</span>
         </Link>
         <Link to="/r-todo-tipo-piel" className="tipo-piel-btn">
-          <img src="https://production.na01.natura.com/on/demandware.static/-/Sites-NatArgentina-Library/default/dw29d76b86/NE%20Rostro/Todos%20os%20tipos.png" alt="Todo Tipo de Piel" className="icono-img" />
+          <img src="https://production.na01.natura.com/on/demandware.static/-/Sites-NatArgentina-Library/default/dw29d76b86/NE%20Rostro/Todos%20os%20tipos.png" alt="Todo Tipo de Piel" className="icono-img-rostro" />
           <span>todo tipo de piel</span>
         </Link>
       </div>
