b74d942b737b3dc16f82f299295d6ba625d727bb
juan marcelo <juanmarcelo84@gmail.com>
2025-07-07 23:24:48 -0300

    zustand solucionado

--- frontend-natura/src/App.jsx
+++ frontend-natura/src/App.jsx
@@ -1,95 +1,97 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Carrito from "./pages/Carrito"
-import CategoriaHombre from "./pages/CategoriaHombre"
-// import CategoriaMujer from "./pages/CategoriaMujer"
-import Categoria from "./pages/Categoria"
-import Infantil from "./pages/Infantil"
-import Admin from "./pages/Admin"
-import Pago from "./pages/Pago"
-import Error404 from "./pages/Error404"
-// import RevistaNatura from './pages/RevistaNatura'
-// import SobreNatura from "./pages/SobreNatura"
-// import Sustentabilidad from "./pages/Sustentabilidad"
-// import QuieroSerConsultor from "./pages/QuieroSerConsultor"
-// import SoyConsultor from "./pages/SoyConsultor"
-// import PorInternet from "./pages/PorInternet"
-// import PorRevista from "./pages/PorRevista"
-import Ayuda from "./pages/Ayuda"
-// import InfoProductos from "./pages/InfoProductos"
-import Blog from "./pages/Blog"
-import Maquillaje from './pages/Maquillaje'
-import Rostro from './pages/Rostro'
-import Perfumeria from './pages/perfumeria'
-import CuidadosDiarios from './pages/CuidadosDiarios'
-import Cabello from './pages/Cabello'
-import Promociones from './pages/Promociones'
-import VerTabla from "./pages/VerTabla"
-import AgregarTabla from './pages/AgregarTabla'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Carrito from "./pages/Carrito";
+import CategoriaHombre from "./pages/CategoriaHombre";
+import Categoria from "./pages/Categoria";
+import Infantil from "./pages/Infantil";
+import Admin from "./pages/Admin";
+import Pago from "./pages/Pago";
+import Error404 from "./pages/Error404";
+import Ayuda from "./pages/Ayuda";
+import Blog from "./pages/Blog";
+import Maquillaje from "./pages/Maquillaje";
+import Rostro from "./pages/Rostro";
+import Perfumeria from "./pages/perfumeria";
+import CuidadosDiarios from "./pages/CuidadosDiarios";
+import Cabello from "./pages/Cabello";
+import Promociones from "./pages/Promociones";
+import VerTabla from "./pages/VerTabla";
+import AgregarTabla from "./pages/AgregarTabla";
 import PrivateRoute from "./components/PrivateRoute";
 import NoAutorizado from "./pages/NoAutorizado";
-import PaginaBusqueda from './pages/PaginaBusqueda'
-
+import PaginaBusqueda from "./pages/PaginaBusqueda";
 
 function App() {
-
   return (
-  
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/404' element={<Error404/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/register' element={<Register/>}></Route>
-        <Route path='/categoria' element={<Categoria/>}></Route>
-        {/* <Route path='/categoria-hombre' element={<CategoriaHombre/>}></Route> */}
-        <Route path='/categorias/:nombre' element={<Categoria/>}></Route>
-        <Route path='/categoria-hombre' element={<CategoriaHombre/>}></Route>
+        {/* Rutas públicas */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/categoria" element={<Categoria />} />
+        <Route path="/categorias/:nombre" element={<Categoria />} />
+        <Route path="/categoria-hombre" element={<CategoriaHombre />} />
+        <Route path="/categoria-infantil" element={<Infantil />} />
         <Route path="/carrito" element={<Carrito />} />
-        {/* <Route path='/categoria-mujer' element={<CategoriaMujer/>}></Route> */}
-        <Route path='/categoria-infantil' element={<Infantil/>}></Route>
-        <Route path='/pago' element={<Pago/>}></Route>
-        {/* {/* <Route path='/revista-natura' element={<RevistaNatura/>}></Route> */} 
-        {/* <Route path='/sobre-natura' element={<SobreNatura/>}></Route> */}
-        {/* <Route path='/sustentabilidad' element={<Sustentabilidad/>}></Route> */}
-        {/* <Route path='/quiero-ser-consultor' element={<QuieroSerConsultor/>}></Route> */}
-        {/* <Route path='/soy-consultor' element={<SoyConsultor/>}></Route>
-        {/* <Route path='/por-revista' element={<PorRevista/>}></Route> */} 
-        {/* {/* <Route path='/por-internet' element={<PorInternet/>}></Route> */} 
-        {/* {/* <Route path='/info-productos' element={<InfoProductos/>}></Route> */} 
-     
-        <Route path='/blog' element={<Blog/>}></Route>
-        <Route path='/maquillaje' element={<Maquillaje/>}></Route>
-        <Route path='/rostro' element={<Rostro/>}></Route>
-        <Route path='/perfumeria' element={<Perfumeria/>}></Route>
-        {/* <Route path='/cuidados-diarios' element={<CuidadosDiarios/>}></Route> */}
-        <Route path='/cabello' element={<Cabello/>}></Route>
-        <Route path='/promociones' element={<Promociones/>}></Route>
-        {/* {/* <Route path='/pro-primera-compra' element={<ProPrimeraCompra/>}></Route> */} 
-        {/* {/* <Route path='/r-piel-oleosa' element={<RPielOleosa/>}></Route> */}
-        {/* <Route path='/r-piel-seca' element={<RPielSeca/>}></Route> */}
-        {/* <Route path='/r-piel-mixta' element={<RPielMixta/>}></Route> */}
-        {/* <Route path='/r-todo-tipo-piel' element={<RTodoTipoPiel/>}></Route> */} 
-        <Route path='/productos/buscar/:termino' element={<PaginaBusqueda/>}></Route>
+        <Route path="/pago" element={<Pago />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/maquillaje" element={<Maquillaje />} />
+        <Route path="/rostro" element={<Rostro />} />
+        <Route path="/perfumeria" element={<Perfumeria />} />
+        <Route path="/cuidados-diarios" element={<CuidadosDiarios />} />
+        <Route path="/cabello" element={<Cabello />} />
+        <Route path="/promociones" element={<Promociones />} />
+        <Route path="/productos/buscar/:termino" element={<PaginaBusqueda />} />
+        <Route path="/ayuda" element={<Ayuda />} />
 
+        {/* Rutas protegidas solo para rol 1 (administrador) */}
+        <Route
+          path="/verTabla/:tabla" 
+          element={
+            <PrivateRoute rolPermitido={1}>
+              <VerTabla />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/agregarTabla/:tabla"
+          element={
+            <PrivateRoute rolPermitido={1}>
+              <AgregarTabla />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute rolPermitido={1}>
+              <Admin />
+            </PrivateRoute>
+          }
+        />
 
-        <Route path='/verTabla/:tabla'  element={<PrivateRoute rolPermitido={1}><VerTabla /></PrivateRoute>} />
-        <Route path='/agregarTabla/:tabla'  element={<PrivateRoute rolPermitido={1}><AgregarTabla /></PrivateRoute>} />
-        <Route path="/admin" element={<PrivateRoute rolPermitido={1}><Admin /></PrivateRoute>}/>
-        <Route path="/perfil" element={<PrivateRoute ><h2>Perfil del usuario</h2></PrivateRoute>}/>
+        {/* Ruta protegida para cualquier usuario logueado */}
+        <Route
+          path="/perfil"
+          element={
+            <PrivateRoute>
+              <h2>Perfil del usuario</h2>
+            </PrivateRoute>
+          }
+        />
+
+        {/* Página de no autorizado y error 404 */}
         <Route path="/no-autorizado" element={<NoAutorizado />} />
-        
-        <Route path='*' element={<Error404/>}></Route>
-        
+        <Route path="/404" element={<Error404 />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
-    
-      
- 
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend-natura/src/components/PrivateRoute.jsx
+++ frontend-natura/src/components/PrivateRoute.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import UsuarioStore from "../store/UsuarioStore"; // ajustá la ruta si está en otra carpeta
 
 const PrivateRoute = ({ children, rolPermitido }) => {
-  let usuario = null;
-  try {
-    usuario = JSON.parse(localStorage.getItem("usuarioLogueado"));
-  } catch {
-    usuario = null;
-  }
+  const { usuario } = UsuarioStore();
 
-  // Validar que usuario exista, sea objeto, tenga rol_id numérico y válido
+  // Validar que usuario exista y tenga rol_id numérico válido
   if (
     !usuario ||
     typeof usuario !== "object" ||
     Array.isArray(usuario) ||
-    usuario === null ||
     typeof usuario.rol_id === "undefined" ||
     usuario.rol_id === null ||
     isNaN(Number(usuario.rol_id))
@@ -22,6 +17,7 @@ const PrivateRoute = ({ children, rolPermitido }) => {
     return <Navigate to="/login" />;
   }
 
+  // Si se pasa un rolPermitido (1 = admin, 2 = cliente), validarlo
   if (rolPermitido && Number(usuario.rol_id) !== Number(rolPermitido)) {
     return <Navigate to="/no-autorizado" />;
   }
@@ -30,3 +26,4 @@ const PrivateRoute = ({ children, rolPermitido }) => {
 };
 
 export default PrivateRoute;
+
